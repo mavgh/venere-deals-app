9ecfb0ba3d068b6cae3e8d7121d1a4c12dec488d
jo-nav <jo-nav@fake.com>
2013-12-06 13:12:15 +0100

    Removed unused code

--- www/js/app/router.js
+++ www/js/app/router.js
@@ -15,8 +15,6 @@ define(function (require) {
 
         routes: {
             "": "home",
-            "employees/:id": "employeeDetails",
-            "employees/:id/reports": "reports",
             "theme/:id": "themeDetails",
             "city/:id/hotels": "hotels"
         },
@@ -24,17 +22,6 @@ define(function (require) {
         home: function () {
             homeView.delegateEvents();
             slider.slidePage(homeView.$el);
-        },
-
-        employeeDetails: function (id) {
-            require(["app/models/employee", "app/views/Employee"], function (models, EmployeeView) {
-                var employee = new models.Employee({id: id});
-                employee.fetch({
-                    success: function (data) {
-                        slider.slidePage(new EmployeeView({model: data}).$el);
-                    }
-                });
-            });
         },
 
         themeDetails: function (id) {
@@ -54,17 +41,6 @@ define(function (require) {
                 city.fetch({
                     success: function (data) {
                         slider.slidePage(new HotelsView({model: data}).$el);
-                    }
-                });
-            });
-        },
-
-        reports: function (id) {
-            require(["app/models/employee", "app/views/Reports"], function (models, ReportsView) {
-                var employee = new models.Employee({id: id});
-                employee.fetch({
-                    success: function (data) {
-                        slider.slidePage(new ReportsView({model: data}).$el);
                     }
                 });
             });
