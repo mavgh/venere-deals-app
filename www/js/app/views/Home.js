5f21a68b9b529d75d522f4becbdcf22d2a9b53b1
v-maaru <v-maaru@ROMS4EAVV0.SEA.CORP.EXPECN.com>
2013-12-06 17:46:45 +0100

    Management of portrait/landscape transition on home

--- www/js/app/views/Home.js
+++ www/js/app/views/Home.js
@@ -5,22 +5,45 @@ define(function (require) {
     var Backbone            = require('backbone'),
         Handlebars          = require('handlebars'),
         models              = require('app/models/theme'),
-        tpl                 = require('text!tpl/Home.html'),
+        tpl_pt              = require('text!tpl/Home.html'),
+        tpl_ls              = require('text!tpl/Home_ls.html'),
 
-        template = Handlebars.compile(tpl);
+        template_pt = Handlebars.compile(tpl_pt),
+        template_ls = Handlebars.compile(tpl_ls),
+        template, view;
 
+        Handlebars.registerHelper('if_even', function(conditional, options) {
+          if((conditional % 2) === 0) {
+            return options.fn(this);
+          } else {
+            return options.inverse(this);
+          }
+        });    
+        
     return Backbone.View.extend({
 
         initialize: function () {
+            window.addEventListener('orientationchange', this.checkOrientationAndRender, false);
+//            $(window).on("resize", this.doOnOrientationChange);
             this.themeList = new models.ThemeCollection();
             this.themeList.fetch({reset: true, data: {}});
-            this.render();
+            view = this;
+            this.checkOrientationAndRender();
         },
 
         render: function () {
             this.$el.html(template({themes: this.themeList.toJSON()}));
             return this;
         },
+        
+        checkOrientationAndRender: function (event) {
+            if (window.orientation === -90 || window.orientation === 90) {
+                template = template_ls;
+            } else {
+                template = template_pt;
+            }
+            view.render();
+        }
     });
 
 });
