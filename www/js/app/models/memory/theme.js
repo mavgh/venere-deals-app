9ebfd76c659f90c80c9a6ec065fffd8f1c5c27f3
Simone Rescio <srescio@expedia.com>
2013-12-24 16:52:07 +0100

    index styles
    added font-face icons, generated 6 color themes hardcoded in theme.js,
    styles for both portrait and landscape

--- www/js/app/models/memory/theme.js
+++ www/js/app/models/memory/theme.js
@@ -7,10 +7,12 @@ define(function (require) {
         City                = require('app/models/city'),
 
         themes = [
-            {"id": 1, "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Winter Sale", "subtitle": "From 06-01-14 to 20-01-14", "start": "2014-01-06", "cityIDs": [1,2,3,4,5]},
-            {"id": 2, "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Christmas Shopping Deals", "subtitle": "From 23-12-13 to 20-01-14", "start": "2013-12-23", "cityIDs": [6,7,8,9,10]},
-            {"id": 3, "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Great value properties", "subtitle": "From 21-12-13 to 20-01-14", "start": "2013-12-21", "cityIDs": [11,9,7,5,3,1]},
-            {"id": 4, "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Offers for New Year", "subtitle": "From 01-01-14 to 20-01-14", "start": "2014-01-01", "cityIDs": [10,8,6,4,2]}
+            {"id": 1, "color":"purple", "img": "http://timoelliott.com/personal/wp-content/uploads/2009/09/eiffel-fireworks.jpg", "title": "New Year Holte Deals", "subtitle": "Where will you enjoy the count down to 2014? From beach vacations to urban gateways, here are some tempting travel offers to ring in 2014 in style.", "start": "2014-01-06", "cityIDs": [1,2,3,4,5]},
+            {"id": 2, "color":"red",    "img": "http://www.coastandcountryfrance.com/property_news/wp-content/uploads/Vienna-Christmas-Market-decorations.jpg", "title": "Christmas Markets", "subtitle": "Mix travel and shopping this Christmas! Find the perfect gifts at the magical Christmas markets of Europe. Or shop til you drop in London, Paris, New York...", "start": "2013-12-23", "cityIDs": [6,7,8,9,10]},
+            {"id": 3, "color":"gold",   "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Christmas Shopping Deals", "subtitle": "Mix travel and shopping this Christmas! Find the perfect gifts at the magical Christmas markets of Europe. Or shop til you drop in London, Paris, New York...", "start": "2013-12-21", "cityIDs": [11,9,7,5,3,1]},
+            {"id": 4, "color":"azure",  "img": "http://i.telegraph.co.uk/multimedia/archive/01125/paris-lights_1125441c.jpg", "title": "Winter Sale", "subtitle": "Mix travel and shopping this Christmas! Find the perfect gifts at the magical Christmas markets of Europe. Or shop til you drop in London, Paris, New York...", "start": "2014-01-01", "cityIDs": [10,8,6,4,2]},
+            {"id": 5, "color":"green",  "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Offers for New Year", "subtitle": "Mix travel and shopping this Christmas! Find the perfect gifts at the magical Christmas markets of Europe. Or shop til you drop in London, Paris, New York...", "start": "2014-01-01", "cityIDs": [10,8,6,4,2]},
+            {"id": 6, "color":"orange",  "img": "http://img.venere.com/img/hotel-deals/Destination-Page/DP-S3-Rome-V1.jpg", "title": "Offers for New Year", "subtitle": "Mix travel and shopping this Christmas! Find the perfect gifts at the magical Christmas markets of Europe. Or shop til you drop in London, Paris, New York...", "start": "2014-01-01", "cityIDs": [10,8,6,4,2]}
             
             
         ],
